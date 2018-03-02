68670cc4158397d16f724b6820e1d14ca63a4649
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2018-03-02 17:29:20 -0300

    Add conflict error

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import BadRequestError from './bad-request-error';
+import ConflictError from './conflict-error';
 import ForbiddenError from './forbidden-error';
 import InternalServerError from './internal-server-error';
 import MeaningError from './meaning-error';
